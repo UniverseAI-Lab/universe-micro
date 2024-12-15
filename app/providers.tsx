f868960e4895aef73f83ae50a1ecf34f80233326
UniverseAI-Lab <kamdoumfranck@proton.me>
2024-12-15 05:09:16 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,7 @@ import { PostHogProvider } from "posthog-js/react";
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
     api_host: "/ingest",
-    ui_host: "https://eu.posthog.com",
+    ui_host: "https://us.i.posthog.com",
     person_profiles: "always", // or 'always' to create profiles for anonymous users as well
   });
 }
