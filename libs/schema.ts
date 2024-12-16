24d4f5cc1c441a46f79738581fe1660869dfcb5f
UniverseAI-Lab <kamdoumfranck@proton.me>
2024-12-16 03:48:28 +0100

    Update schema.ts

--- libs/schema.ts
+++ libs/schema.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+// Example message schema
 export const exampleMessageSchema = z.object({
   heading: z
     .string()
@@ -19,45 +20,32 @@ export const exampleMessageSchemaObject = z.object({
 
 export type ExampleMessage = z.infer<typeof exampleMessageSchema>;
 
-export const countryCodeSchema = z.enum([
-  "AR",
-  "AU",
-  "AT",
-  "BE",
-  "BR",
-  "CA",
-  "CL",
-  "DK",
-  "FI",
-  "FR",
-  "DE",
-  "HK",
-  "IN",
-  "ID",
-  "IT",
-  "JP",
-  "KR",
-  "MY",
-  "MX",
-  "NL",
-  "NZ",
-  "NO",
-  "CN",
-  "PL",
-  "PT",
-  "PH",
-  "RU",
-  "SA",
-  "ZA",
-  "ES",
-  "SE",
-  "CH",
-  "TW",
-  "TH",
-  "TR",
-  "GB",
-  "US",
-]);
+// Full global list of ISO 3166-1 alpha-2 country codes
+const isoCountryCodes = [
+  "AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW",
+  "AU", "AT", "AZ", "BS", "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT",
+  "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "CV", "KH",
+  "CM", "CA", "KY", "CF", "TD", "CL", "CN", "CX", "CC", "CO", "KM", "CG", "CD",
+  "CK", "CR", "HR", "CU", "CW", "CY", "CZ", "CI", "DK", "DJ", "DM", "DO", "EC",
+  "EG", "SV", "GQ", "ER", "EE", "SZ", "ET", "FK", "FO", "FJ", "FI", "FR", "GF",
+  "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU",
+  "GT", "GG", "GN", "GW", "GY", "HT", "HM", "VA", "HN", "HK", "HU", "IS", "IN",
+  "ID", "IR", "IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE",
+  "KI", "KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT",
+  "LU", "MO", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT",
+  "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA", "NR", "NP",
+  "NL", "NC", "NZ", "NI", "NE", "NG", "NU", "NF", "MK", "MP", "NO", "OM", "PK",
+  "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "RE",
+  "RO", "RU", "RW", "BL", "SH", "KN", "LC", "MF", "PM", "VC", "WS", "SM", "ST",
+  "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS",
+  "SS", "ES", "LK", "SD", "SR", "SJ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH",
+  "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE",
+  "GB", "US", "UM", "UY", "UZ", "VU", "VE", "VN", "VG", "VI", "WF", "EH", "YE",
+  "ZM", "ZW",
+];
+
+// Make schema dynamic for every country
+export const countryCodeSchema = z.enum(isoCountryCodes);
 
 export type CountryCode = z.infer<typeof countryCodeSchema>;
 
